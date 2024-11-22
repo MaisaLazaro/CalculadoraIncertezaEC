2b9abb771227a42ac9f84f3fc9149ae1577802a7
MaisaLazaro <maisaclazaro@gmail.com>
2024-11-22 17:25:55 -0300

    Add files via upload

--- CalculadoraIncertezaEC.py
+++ CalculadoraIncertezaEC.py
@@ -27,7 +27,7 @@ def set_background(png_file):
 
 # Chame a função com o caminho do arquivo de imagem
 set_background(
-    r'C:\Users\Maisa\Documents\Python_projetos\static\imagemFundo7.png')
+    r'static\imagemFundo7.png')
 
 # Título do aplicativo
 st.markdown("<br><br>", unsafe_allow_html=True)
